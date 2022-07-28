ef2db3bf0bf6532ced0846f8df7361151f5a3137
Jordan <jordanrafael92@gmail.com>
2022-07-28 10:34:35 -0300

    Created project

---
+++ backend/src/main/java/com/jrsilva/CallRegistration/CallRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.jrsilva.CallRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallRegistrationApplication.class, args);
+	}
+
+}
