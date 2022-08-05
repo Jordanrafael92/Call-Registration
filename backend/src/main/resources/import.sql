9ffa5571defcf52fa878a5d2d55038fde1076192
Jordan <jordanrafael92@gmail.com>
2022-08-05 16:33:18 -0300

    Create seed database

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into tb_person(cpf,name) values (54705226011, 'Debora Velasques Oleiro');
+insert into tb_person(cpf,name) values (29613299068, 'Jónatas Espargosa Vilalobos');
+insert into tb_person(cpf,name) values (29595417076, 'Suria Baião Macedo');
+insert into tb_person(cpf,name) values (33793439003, 'Samara Morão Boto');
