2a9947c1007aa651abb9cb9483b8634da04bf752
Jordan <jordanrafael92@gmail.com>
2022-08-13 11:52:16 -0300

    Created class UserResource (endpoint)

---
+++ backend/src/main/java/com/jrsilva/callRegistration/resources/UserResource.java
@@ -0,0 +1,26 @@
+package com.jrsilva.callRegistration.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jrsilva.callRegistration.dto.UserDTO;
+import com.jrsilva.callRegistration.services.UserService;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+	@Autowired
+	private UserService service;
+	
+	@GetMapping
+	public ResponseEntity<List<UserDTO>> findAll() {
+		List<UserDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+}
