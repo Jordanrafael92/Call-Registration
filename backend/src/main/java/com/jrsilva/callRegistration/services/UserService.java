879c12475ffcede64aa35492ccc0ae3c3415ae09
Jordan <jordanrafael92@gmail.com>
2022-08-13 11:44:15 -0300

    [fix] Added notation @Service

--- backend/src/main/java/com/jrsilva/callRegistration/services/UserService.java
+++ backend/src/main/java/com/jrsilva/callRegistration/services/UserService.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jrsilva.callRegistration.dto.UserDTO;
 import com.jrsilva.callRegistration.repositories.UserRepository;
 
+@Service
 public class UserService {
 	
 	@Autowired
