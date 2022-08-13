d6049b0d2c4978ac7d009069ab5b2f3bb9b6202a
Jordan <jordanrafael92@gmail.com>
2022-08-13 11:16:07 -0300

    Added records to the database - Table TB_USER

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -2,3 +2,7 @@ insert into tb_person(cpf,name) values (54705226011, 'Debora Velasques Oleiro');
 insert into tb_person(cpf,name) values (29613299068, 'Jónatas Espargosa Vilalobos');
 insert into tb_person(cpf,name) values (29595417076, 'Suria Baião Macedo');
 insert into tb_person(cpf,name) values (33793439003, 'Samara Morão Boto');
+insert into tb_user(id_person, login, password, admin) values (1, 'Debora', '1234', false);
+insert into tb_user(id_person, login, password, admin) values (2, 'Jonatas', '1234', false);
+insert into tb_user(id_person, login, password, admin) values (3, 'Suria', '1234', false);
+insert into tb_user(id_person, login, password, admin) values (4, 'Samara', '1234', true);
