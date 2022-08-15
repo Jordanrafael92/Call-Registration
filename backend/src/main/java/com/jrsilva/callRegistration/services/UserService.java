78d4c535a636564f249d8a2351fc5d4fa5993db1
Jordan <jordanrafael92@gmail.com>
2022-08-15 20:09:50 -0300

    Refactor class User

--- backend/src/main/java/com/jrsilva/callRegistration/services/UserService.java
+++ backend/src/main/java/com/jrsilva/callRegistration/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.jrsilva.callRegistration.dto.UserDTO;
 import com.jrsilva.callRegistration.repositories.UserRepository;
+import com.jrsilva.callRegistration.entities.User;
 
 @Service
 public class UserService {
@@ -18,7 +19,7 @@ public class UserService {
 	
 	@Transactional(readOnly = true)
 	public List<UserDTO> findAll() {
-		List<com.jrsilva.callRegistration.entities.User> list = repository.findAll();
+		List<User> list = repository.findAll();
 		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 	}
 }
