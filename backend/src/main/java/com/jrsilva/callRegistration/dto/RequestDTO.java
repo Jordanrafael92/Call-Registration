0c97df20cc83b34cd1db8e1ada9862fe2e0d45cb
Jordan <jordanrafael92@gmail.com>
2022-08-15 19:47:32 -0300

    Created class DTO - entity Request

--- backend/src/main/java/com/jrsilva/callRegistration/dto/RequestDTO.java
+++ backend/src/main/java/com/jrsilva/callRegistration/dto/RequestDTO.java
@@ -16,5 +16,52 @@ public class RequestDTO implements Serializable{
 
 	@Enumerated(EnumType.STRING)
 	private DescriptionStatus status = DescriptionStatus.ABERTO;
+	
+	public RequestDTO() {
+		
+	}
+
+	public RequestDTO(Long idRequest, Long idUser, String description, DescriptionStatus status) {
+		this.idRequest = idRequest;
+		this.idUser = idUser;
+		this.description = description;
+		this.status = status;
+	}
+
+	public Long getIdRequest() {
+		return idRequest;
+	}
+
+	public void setIdRequest(Long idRequest) {
+		this.idRequest = idRequest;
+	}
+
+	public Long getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(Long idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public DescriptionStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(DescriptionStatus status) {
+		this.status = status;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
 }
