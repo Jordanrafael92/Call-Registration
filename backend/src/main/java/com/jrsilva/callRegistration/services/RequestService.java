fbe4601d9bbe9383ed7d24c2520160167e6be256
Jordan <jordanrafael92@gmail.com>
2022-08-21 11:04:13 -0300

    Created Find By Id - RequestService

--- backend/src/main/java/com/jrsilva/callRegistration/services/RequestService.java
+++ backend/src/main/java/com/jrsilva/callRegistration/services/RequestService.java
@@ -1,15 +1,18 @@
 package com.jrsilva.callRegistration.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jrsilva.callRegistration.dto.RequestDTO;
-import com.jrsilva.callRegistration.repositories.RequestRepository;
 import com.jrsilva.callRegistration.entities.Request;
+import com.jrsilva.callRegistration.repositories.RequestRepository;
 
 @Service
 public class RequestService {
@@ -21,5 +24,12 @@ public class RequestService {
 	public List<RequestDTO> findAll() {
 		List<Request> list = repository.findAll();
 		return list.stream().map(x -> new RequestDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public RequestDTO findById(Long id) {
+		Optional<Request> obj = repository.findById(id);
+		Request entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found - (Request)"));
+		return new RequestDTO(entity);
 	}
 }
