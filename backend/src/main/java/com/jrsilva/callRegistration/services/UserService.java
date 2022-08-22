5e1615e9e04749f5934c1fc6aa1ffbbe06b1ce26
Jordan <jordanrafael92@gmail.com>
2022-08-22 09:22:50 -0300

    Created method insert - class UserService

--- backend/src/main/java/com/jrsilva/callRegistration/services/UserService.java
+++ backend/src/main/java/com/jrsilva/callRegistration/services/UserService.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jrsilva.callRegistration.dto.UserDTO;
-import com.jrsilva.callRegistration.repositories.UserRepository;
 import com.jrsilva.callRegistration.entities.User;
+import com.jrsilva.callRegistration.repositories.UserRepository;
 
 @Service
 public class UserService {
@@ -22,4 +22,15 @@ public class UserService {
 		List<User> list = repository.findAll();
 		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 	}
+	
+	@Transactional
+	public UserDTO insert(UserDTO dto) {
+		User entity = new User();
+		entity.setIdPerson(dto.getIdPerson());
+		entity.setLogin(dto.getLogin());
+		entity.setPassword(dto.getPassword());
+		entity.setAdmin(dto.getAdmin());
+		return new UserDTO(entity);
+	}
+	
 }
